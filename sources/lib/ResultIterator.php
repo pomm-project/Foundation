7837e584a8431d1e90308540c5b4a288325405d5
Grégoire HUBERT <hubert.greg@gmail.com>
2014-09-24 16:02:18 +0200

    Added ResultIterator.

---
+++ sources/lib/ResultIterator.php
@@ -0,0 +1,317 @@
+<?php
+/*
+ * This file is part of the Pomm's Foundation package.
+ *
+ * (c) 2014 Grégoire HUBERT <hubert.greg@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PommProject\Foundation;
+
+use PommProject\Foundation\Exception\ConnectionException;
+use PommProject\Foundation\Converter\ConverterHolder;
+
+/**
+ * ResultIterator
+ *
+ * Iterator on database results.
+ *
+ * @package Foundation
+ * @copyright 2014 Grégoire HUBERT
+ * @author Grégoire HUBERT
+ * @license X11 {@link http://opensource.org/licenses/mit-license.php}
+ * @see \Iterator
+ * @see \Countable
+ */
+class ResultIterator implements \Iterator, \Countable
+{
+    private   $position;
+    protected $result_resource;
+    protected $converter_holder;
+    protected $types = [];
+
+    /**
+     * __construct
+     *
+     * Constructor
+     *
+     * @access public
+     * @param  resuource       $result_resource
+     * @param  ConverterHolder $converter_holder
+     * @return void
+     */
+    public function __construct($result_resource, ConverterHolder $converter_holder)
+    {
+        $this->result_resource  = $result_resource;
+        $this->converter_holder = $converter_holder;
+        $this->position         = $this->result_resource === false ? null : 0;
+        $this->getTypes();
+    }
+
+    /**
+     * __destruct
+     *
+     * Closes the cursor when the collection is cleared.
+     */
+    public function __destruct()
+    {
+        @pg_free_result($this->result_resource);
+    }
+
+    /**
+     * get
+     *
+     * Return a particular result. If the result cannot be read, false is
+     * returned, otherwise, the according FlexibleEntity is returned.
+     * pg_fetch_array is muted because it produces untrappable warnings on
+     * errors.
+     *
+     * @param  integer $index
+     * @return array
+     */
+    public function get($index)
+    {
+        $values = @pg_fetch_array($this->result_resource, $index, \PGSQL_NUM);
+
+        if ($values === false) {
+            throw new \OutOfBoundsException(sprintf("Cannot jump to non existing row %d.", $index));
+        }
+
+        return $this->parseRow($values);
+    }
+
+    /**
+     * parseRow
+     *
+     * Convert values from Pg.
+     *
+     * @access protected
+     * @param  array $values
+     * @return array
+     */
+    protected function parseRow(array $values)
+    {
+        $output_values = [];
+
+        foreach($values as $index => $value) {
+            $output_values[pg_field_name($this->result_resource, $index)] =
+                $this->convertField($index, $value) ;
+        }
+
+        return $output_values;
+    }
+
+    /**
+     * convertField
+     *
+     * Return converted value for a result field.
+     *
+     * @access protected
+     * @param int    $field_no
+     * @param string $value
+     * @return mixed
+     */
+    protected function convertField($field_no, $value)
+    {
+        return $this
+            ->converter_holder
+            ->getConverterForType($this->types[$field_no])
+            ->fromPg($value)
+            ;
+    }
+
+    /**
+     * getTypes
+     *
+     * Get the result types from the result handler.
+     *
+     * @access protectd
+     * @return ResultIterator $this
+     */
+    protected function getTypes()
+    {
+        for($i = 0; $i < pg_num_fields($this->result_resource); $i++) {
+            $this->types[$i] = pg_field_type($this->result_resource, $i);
+        }
+
+        return $this;
+    }
+
+    /**
+     * has
+     *
+     * Return true if the given index exists false otherwise.
+     *
+     * @param  integer $index
+     * @return boolean
+     */
+    public function has($index)
+    {
+        return (bool) ($index < $this->count());
+    }
+
+    /**
+     * count
+     *
+     * @see \Countable
+     * @return integer
+     */
+    public function count()
+    {
+        return pg_num_rows($this->result_resource);
+    }
+
+    /**
+     * rewind
+     *
+     * @see \Iterator
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    /**
+     * current
+     *
+     * @see \Iterator
+     */
+    public function current()
+    {
+        return $this->get($this->position);
+    }
+
+    /**
+     * key
+     *
+     * @see \Iterator
+     */
+    public function key()
+    {
+        return $this->position;
+    }
+
+    /**
+     * next
+     *
+     * @see \Iterator
+     */
+    public function next()
+    {
+        ++$this->position;
+    }
+
+    /**
+     * valid
+     *
+     * @see \Iterator
+     * @return Boolean
+     */
+    public function valid()
+    {
+        return $this->has($this->position);
+    }
+
+    /**
+     * isFirst
+     * Is the iterator on the first element ?
+     *
+     * @return Boolean
+     */
+    public function isFirst()
+    {
+        return $this->position === 0;
+    }
+
+    /**
+     * isLast
+     *
+     * Is the iterator on the last element ?
+     *
+     * @return Boolean
+     */
+    public function isLast()
+    {
+        return $this->position === $this->count() - 1;
+    }
+
+    /**
+     * isEmpty
+     *
+     * Is the collection empty (no element) ?
+     *
+     * @return Boolean
+     */
+    public function isEmpty()
+    {
+        return pg_num_rows($this->result_resource) === 0;
+    }
+
+    /**
+     * isEven
+     *
+     * Is the iterator on an even position ?
+     *
+     * @return Boolean
+     */
+    public function isEven()
+    {
+        return ($this->position % 2) === 0;
+    }
+
+    /**
+     * isOdd
+     *
+     * Is the iterator on an odd position ?
+     *
+     * @return Boolean
+     */
+    public function isOdd()
+    {
+        return ($this->position % 2) === 1;
+    }
+
+    /**
+     * getOddEven
+     *
+     * Return 'odd' or 'even' depending on the element index position.
+     * Useful to style list elements when printing lists to do
+     * <li class="line_<?php $list->getOddEven() ?>">.
+     *
+     * @return String
+     */
+    public function getOddEven()
+    {
+        return $this->position % 2 ? 'odd' : 'even';
+    }
+
+    /**
+     * slice
+     *
+     * Extract an array of values for one column.
+     *
+     * @param  string $field
+     * @return array  values
+     */
+    public function slice($field)
+    {
+        if ($this->isEmpty()) {
+            return [];
+        }
+
+        $field_no = pg_field_num($this->result_resource, $field);
+
+        if ($field_no === -1) {
+            throw new \InvalidArgumentException(sprintf("No such field '%s' in result set.", $field));
+        }
+
+        $values = [];
+
+        foreach(pg_fetch_all_columns($this->result_resource, $field_no) as $incoming_value) {
+            $values[] = $this->convertField($field_no, $incoming_value);
+        }
+
+        return $values;
+    }
+}
---
+++ sources/tests/Unit/ResultIterator.php
@@ -0,0 +1,168 @@
+<?php
+/*
+ * This file is part of the PommProject/Foundation package.
+ *
+ * (c) 2014 Grégoire HUBERT <hubert.greg@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PommProject\Foundation\Test\Unit;
+
+use Atoum;
+use PommProject\Foundation\Converter\ConverterHolder;
+use PommProject\Foundation\DatabaseConfiguration;
+use PommProject\Foundation\Session;
+use PommProject\Foundation\Converter;
+
+class ResultIterator extends Atoum
+{
+    protected $connection;
+
+    protected function getPikaSql()
+    {
+        return <<<SQL
+select
+    p.id,
+    p.pika
+from
+  (values
+    (1::int4, 'a'::text),
+    (2, 'b'),
+    (3, 'c'),
+    (4, 'd')
+  ) p (id, pika)
+SQL;
+    }
+
+    protected function getConverterHolder()
+    {
+        $holder = (new ConverterHolder())
+            ->registerConverter('Boolean', new Converter\PgBoolean(), ['bool'])
+            ->registerConverter('Number', new Converter\PgNumber(), ['int2', 'int4', 'int8', 'numeric', 'float4', 'float8'])
+            ->registerConverter('String', new Converter\PgString(), ['varchar', 'char', 'text'])
+            ;
+
+        $holder->registerConverter('Array', new Converter\PgArray($holder), []);
+
+        return $holder;
+    }
+
+    protected function getResultResource($sql, array $params = [])
+    {
+        if ($this->connection === null) {
+            $this->connection = new Session(new DatabaseConfiguration($GLOBALS['pomm_db1']));
+        }
+
+        if (pg_send_query_params($this->connection->getHandler(), $sql, $params) === false) {
+            throw new \RunTimeException(sprintf("Error while querying '%s'", $sql));
+        }
+
+        return pg_get_result($this->connection->getHandler());
+    }
+
+    public function testConstructor()
+    {
+        $iterator = $this->newTestedInstance(
+            $this->getResultResource("select true::boolean"),
+            $this->getConverterHolder()
+        );
+
+        $this
+            ->object($iterator)
+            ->isInstanceOf('\PommProject\Foundation\ResultIterator')
+            ->isInstanceOf('\Countable')
+            ->isInstanceOf('\Iterator')
+            ;
+    }
+
+    public function testGet()
+    {
+        $sql = $this->getPikaSql();
+        $iterator = $this->newTestedInstance(
+            $this->getResultResource($sql),
+            $this->getConverterHolder()
+        );
+
+        $this
+            ->array($iterator->get(0))
+            ->isIdenticalTo(['id' => 1, 'pika' => 'a'])
+            ->array($iterator->get(2))
+            ->isIdenticalTo(['id' => 3, 'pika' => 'c'])
+            ->array($iterator->get(1))
+            ->isIdenticalTo(['id' => 2, 'pika' => 'b'])
+            ->exception(function() use ($iterator) { return $iterator->get(5); })
+            ->isInstanceOf('\OutOfBoundsException')
+            ->message->contains('Cannot jump to non existing row')
+            ;
+    }
+
+    public function testHasAndCount()
+    {
+        $sql = $this->getPikaSql();
+        $iterator = $this->newTestedInstance(
+            $this->getResultResource($sql),
+            $this->getConverterHolder()
+        );
+
+        $this
+            ->integer($iterator->count())
+            ->isEqualTo(4)
+            ->boolean($iterator->has(1))
+            ->isTrue()
+            ->boolean($iterator->has(4))
+            ->isFalse()
+            ;
+    }
+
+    public function testIterator()
+    {
+        $sql = $this->getPikaSql();
+        $iterator = $this->newTestedInstance(
+            $this->getResultResource($sql),
+            $this->getConverterHolder()
+        );
+
+        foreach($iterator as $index => $element) {
+            if ($index === 0) {
+                $this
+                    ->boolean($iterator->isFirst())
+                    ->isTrue();
+            } elseif ($index === $iterator->count() - 1) {
+                $this
+                    ->boolean($iterator->isLast())
+                    ->isTrue();
+            } else {
+                $this
+                    ->boolean($iterator->isFirst())
+                    ->isFalse()
+                    ->boolean($iterator->isLast())
+                    ->isFalse()
+                    ;
+            }
+            $this
+                ->integer($element['id'])
+                ->isEqualTo($index + 1)
+                ;
+        }
+    }
+
+    public function testSlice()
+    {
+        $sql = $this->getPikaSql();
+        $iterator = $this->newTestedInstance(
+            $this->getResultResource($sql),
+            $this->getConverterHolder()
+        );
+
+        $this
+            ->array($iterator->slice('pika'))
+            ->isIdenticalTo(['a', 'b', 'c', 'd'])
+            ->array($iterator->slice('id'))
+            ->isIdenticalTo([1, 2, 3, 4])
+            ->exception(function() use ($iterator) { return $iterator->slice('no_such_key'); })
+            ->isInstanceOf('\InvalidArgumentException')
+            ->message->contains('No such field')
+            ;
+    }
+}

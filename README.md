60eb94777877cfcfe47f674579dce362a24f7313
Grégoire HUBERT <hubert.greg@gmail.com>
2014-09-25 17:18:41 +0200

    Added Where class.

--- README.md
+++ README.md
@@ -1,5 +1,93 @@
 # Foundation
 
-This is the foundation component for the Pomm database framework.
+This is the foundation component for the Pomm database framework. It works only with PHP >= 5.4.4.
 
 This is experimental software and it may be broken or not non functional. If you are looking for a stable library, look at [Pomm 1.x](http://www.pomm-project.org).
+
+## What is that ?
+
+`Foundation` is the main block of Pomm database framework. It handles connection configuration and sessions.
+
+The easiest way to open a connection to the database server:
+
+```php
+<?php
+
+/*
+ ... autoloading stuff ..
+*/
+
+// instantiate the service with the configuration as parameter:
+$pomm = new \PommProject\Foundation\Pomm(['my_db' => ['dsn' => 'pgsql://greg/greg']]);
+
+// get a session from the service:
+$session = $pomm['my_db'];
+```
+
+## Sessions, clients and poolers
+
+The `Session` instance is the keystone of Foundation. It is a client manager for the database connection handler. A client is a class that needs to interact with the database. All model files are in a way clients of a session. By example, prepared statements are clients of a session. To be able to do its job it needs to do the following:
+
+ * Check if a prepared query already exist
+ * If not, instantiate a PreparedQuery and register as client to the session.
+ * Execute the query.
+
+This is a bit complicated, in order to make developers life a bit more easy, interactions between clients and session may be delegated to dedicated poolers. All the following steps then become:
+
+```php
+<?php
+// ...
+
+$pomm = new \PommProject\Foundation\Pomm(['my_db' => ['dsn' => 'pgsql://greg/greg']]);
+$pomm['my_db']
+    ->getPoolerForType('prepared_query')
+    ->getClient('select * from student where age > $*')
+    ->execute([20])
+    ;
+```
+
+This can also be written as the following:
+
+```php
+<?php
+// ...
+
+$pomm = new \PommProject\Foundation\Pomm(['my_db' => ['dsn' => 'pgsql://greg/greg']]);
+$pomm['my_db']
+    ->getPreparedQuery('select * from student where age > $*')
+    ->execute([20])
+    ;
+```
+
+The point here is to understand that the instantiated clients are automatically reused when they are called several times. Clients are shutdown properly when the session is destroyed by PHP. The second strong point of this system is that all clients own a pointer to the session. So it can use other clients from it.
+
+## How to develop with Foundation ?
+
+In most cases you are interested in using structures and entities from a database schema. This is the job of the Model Manager client provided with another package (yet to create). There may be cases where Model Manager would not fit the job, especially in case of legacy applications (among others):
+
+ * Access to the database are stored procedures only.
+ * SQL queries are stored in separate files in different directories.
+ * You want stats, not entity related computations.
+ * …
+
+It is easy to develop client classes and poolers to interact with the session, it is just a matter of implementing the `ClientInterface` or `ClientPoolerInterface` and register them in the session.
+
+## QueryManager and converter system.
+
+The `QueryManager` is the class that handles the way the `Session` class performs queries. By default, it uses the `SimpleQueryManager` which is just a wrapper around a parametrized query:
+
+```php
+<?php
+//...
+$pomm = new \PommProject\Foundation\Pomm(['my_db' => ['dsn' => 'pgsql://greg/greg']]);
+$iterator = $pomm['my_db']
+    ->query('select * from student where age > $*', [20]);
+
+foreach($iterator as $r) {
+    printf("Student id = '%d', age = '%d', name = '%s'.\n",, $r['id'], $r['age'], $r['name']);
+}
+```
+
+All values are **converted** using Foundation converter system.
+
+It is possible to override the default query manager with your own as soon as it implements `QueryManagerInterface`.
---
+++ sources/lib/Where.php
@@ -0,0 +1,324 @@
+<?php
+/*
+ * This file is part of the PommProject/Foundation package.
+ *
+ * (c) 2014 Grégoire HUBERT <hubert.greg@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PommProject\Foundation;
+
+
+/**
+ * Where
+ *
+ * This class represents a WHERE clause of a SQL statement. It deals with AND &
+ * OR operator you can add using handy methods. This allows you to build
+ * queries dynamically.
+ *
+ * @package Foundation
+ * @copyright 2014 Grégoire HUBERT
+ * @author Grégoire HUBERT <hubert.greg@gmail.com>
+ * @license X11 {@link http://opensource.org/licenses/mit-license.php}
+ */
+class Where
+{
+    public $stack = [];
+    public $element;
+    public $values = [];
+    public $operator;
+
+    /**
+     * create
+     *
+     * A constructor you can chain from.
+     *
+     * @static
+     * @access public
+     * @param  string $element
+     * @param  array  $values
+     * @return Where
+     */
+    public static function create($element = null, array $values = [])
+    {
+        return new self($element, $values);
+    }
+
+    /**
+     * createWhereIn
+     *
+     * Create an escaped IN clause.
+     *
+     * @static
+     * @access public
+     * @param  string $element
+     * @param  array  $values
+     * @return Where
+     */
+    public static function createWhereIn($element, array $values)
+    {
+        $escape = function ($values) use (&$escape) {
+                $escaped_values = [];
+
+                foreach($values as $value) {
+                    if (is_array($value)) {
+                        $escaped_values[] = sprintf("(%s)", join(', ', $escape($value)));
+                    } else {
+                        $escaped_values[] = '$*';
+                    }
+                }
+
+                return $escaped_values;
+            };
+
+        $get_values = function($values) {
+                $array = [];
+
+                foreach(new \RecursiveIteratorIterator(new \RecursiveArrayIterator($values)) as $value) {
+                    $array[] = $value;
+                }
+
+                return $array;
+            };
+
+        return new self(sprintf("%s IN (%s)", $element, join(", ", $escape($values))), $get_values($values));
+    }
+
+    /**
+     * __construct
+     *
+     * @access public
+     * @param string $element  (optional)
+     * @param array  $values   (optional)
+     */
+    public function __construct($element = null, array $values = [])
+    {
+        if (!is_null($element)) {
+            $this->element = $element;
+            $this->values = $values;
+        }
+    }
+
+    /**
+     * setOperator
+     *
+     * is it an AND or an OR ?
+     * or something else.
+     * XOR can be expressed as "A = !B"
+     *
+     * @access public
+     * @param  string $operator
+     * @return Where
+     */
+    public function setOperator($operator)
+    {
+        $this->operator = $operator;
+
+        return $this;
+    }
+
+    /**
+     * isEmpty
+     *
+     * is it a fresh brand new object ?
+     *
+     * @access public
+     * @return Boolean
+     */
+    public function isEmpty()
+    {
+        return (bool) (is_null($this->element) && count($this->stack) == 0);
+    }
+
+    /**
+     * transmute
+     *
+     * Absorbing another Where instance.
+     *
+     * @access public
+     * @param  Where $where
+     * @return Where $this
+     */
+    public function transmute(Where $where)
+    {
+        $this->stack    = $where->stack;
+        $this->element  = $where->element;
+        $this->operator = $where->operator;
+        $this->values   = $where->values;
+    }
+
+    /**
+     * addWhere
+     *
+     * You can add a new WHERE clause with your own operator.
+     *
+     * @access public
+     * @param  mixed  $element
+     * @param  array  $values
+     * @param  string $operator
+     * @return Where
+     */
+    public function addWhere($element, array $values, $operator)
+    {
+        if (!$element instanceof Where) {
+            $element = new self($element, $values);
+        }
+
+        if ($element->isEmpty()) return $this;
+        if ($this->isEmpty()) {
+            $this->transmute($element);
+
+            return $this;
+        }
+
+        if ($this->hasElement()) {
+            $this->stack = [new self($this->getElement(), $this->values), $element];
+            $this->element = null;
+            $this->values = [];
+        } else {
+            if ($this->operator == $operator) {
+                $this->stack[] = $element;
+            } else {
+                $this->stack = [
+                    self::create()
+                        ->setStack($this->stack)
+                        ->setOperator($this->operator),
+                    $element
+                ];
+            }
+        }
+
+        $this->operator = $operator;
+
+        return $this;
+    }
+
+    /**
+     * andWhere
+     *
+     * Or use a ready to use AND where clause.
+     *
+     * @access public
+     * @param  mixed $element
+     * @param  array $values
+     * @return Where
+     */
+    public function andWhere($element, array $values = [])
+    {
+        return $this->addWhere($element, $values, 'AND');
+    }
+
+    /**
+     * orWhere
+     *
+     * @access public
+     * @param  mixed $element
+     * @param  array $values
+     * @return Where
+     */
+    public function orWhere($element, array $values = [])
+    {
+        return $this->addWhere($element, $values, 'OR');
+    }
+
+    /**
+     * setStack
+     *
+     * @access public
+     * @param  array $stack
+     * @return Where
+     */
+    public function setStack(array $stack)
+    {
+        $this->stack = $stack;
+
+        return $this;
+    }
+
+    /**
+     * __toString
+     *
+     * where your SQL statement is built.
+     *
+     * @access public
+     * @return string
+     */
+    public function __toString()
+    {
+        if ($this->isEmpty()) {
+            return 'true';
+        } else {
+            return $this->parse();
+        }
+    }
+
+    /**
+     * hasElement
+     *
+     * @access public
+     * @return Boolean
+     */
+    public function hasElement()
+    {
+        return ! is_null($this->element);
+    }
+
+    /**
+     * getElement
+     *
+     * @access public
+     * @return string
+     */
+    public function getElement()
+    {
+        return $this->element;
+    }
+
+    /**
+     * parse
+     *
+     * @access protected
+     * @return string
+     */
+    protected function parse()
+    {
+        if ($this->hasElement()) {
+            return $this->getElement();
+        }
+
+        $stack = [];
+        foreach ($this->stack as $offset => $where) {
+            $stack[$offset] = $where->parse();
+        }
+
+        return sprintf('(%s)', join(sprintf(' %s ', $this->operator), $stack));
+    }
+
+    /**
+     * getValues
+     *
+     * Get all the values back for the prepared statement.
+     *
+     * @access public
+     * @return array
+     */
+    public function getValues()
+    {
+        if ($this->isEmpty()) {
+            return [];
+        }
+
+        if ($this->hasElement()) {
+            return $this->values;
+        }
+
+        $values = [];
+
+        foreach($this->stack as $offset => $where) {
+            $values = array_merge($values, $where->getValues());
+        }
+
+        return $values;
+    }
+}

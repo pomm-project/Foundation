62bdab21fa80d79b4f91512a2cc8c2e2c507e2f6
Grégoire HUBERT <hubert.greg@gmail.com>
2014-10-14 17:20:53 +0200

    Travis integration.

--- .bootstrap.atoum.php
+++ .bootstrap.atoum.php
@@ -1,3 +1,14 @@
 <?php
 $loader = require __DIR__ . '/vendor/autoload.php';
-require __DIR__.'/sources/tests/config.php';
+$file = __DIR__.'/sources/tests/config.php';
+
+if (file_exists($file)) {
+    // custom configuration
+    require $file;
+} else {
+    // we are using travis configuration by default
+    $GLOBALS['pomm_db1'] = [
+        'dsn' => 'pgsql://postgres@127.0.0.1/travis_ci_test'
+        ];
+}
+

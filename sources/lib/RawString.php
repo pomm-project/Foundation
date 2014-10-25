c2dfc07917c677e4bf431dea5df1e4f85e587b3f
Grégoire HUBERT <hubert.greg@gmail.com>
2014-10-25 15:05:38 +0200

    Added RawString.

---
+++ sources/lib/RawString.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of the Pomm's Foundation package.
+ *
+ * (c) 2014 Grégoire HUBERT <hubert.greg@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PommProject\Foundation;
+
+/**
+ * RowString
+ *
+ * Expression escaper.
+ *
+ * This type allows to pass raw expressions as is to the database. Useful to
+ * call functions or database expressions.
+ *
+ * @package Foundation
+ * @copyright 2014 Grégoire HUBERT
+ * @author Grégoire HUBERT
+ * @license X11 {@link http://opensource.org/licenses/mit-license.php}
+ */
+class RawString
+{
+    protected $expression;
+
+    /**
+     * __construct
+     *
+     * Create a RawString.
+     *
+     * @access public
+     * @param  string $expression
+     * @return null
+     */
+    public function __construct($expression)
+    {
+        $this->expression = $expression;
+    }
+
+    /**
+     * __toString
+     *
+     * String cast this instance.
+     *
+     * @access public
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->expression;
+    }
+}

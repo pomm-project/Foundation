3e7152210fa5fbf460c457dbb91b036105957474
Grégoire HUBERT <hubert.greg@gmail.com>
2016-10-12 10:57:12 +0200

    add a result iteratir interface

--- sources/lib/ResultIterator.php
+++ sources/lib/ResultIterator.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Pomm's Foundation package.
  *
- * (c) 2014 - 2015 Grégoire HUBERT <hubert.greg@gmail.com>
+ * (c) 2014 - 2016 Grégoire HUBERT <hubert.greg@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,20 +14,20 @@ use PommProject\Foundation\Session\ResultHandler;
 /**
  * ResultIterator
  *
- * Iterator on database results.
+ * This is a simple iterator on database results. It manages a result database
+ * resource.
  *
  * @package   Foundation
- * @copyright 2014 - 2015 Grégoire HUBERT
+ * @copyright 2014 - 2016 Grégoire HUBERT
  * @author    Grégoire HUBERT
  * @license   X11 {@link http://opensource.org/licenses/mit-license.php}
- * @see       \Iterator
- * @see       \Countable
+   @see       ResultIteratorInterface
  * @see       \JsonSerializable
  */
-class ResultIterator implements \Iterator, \Countable, \JsonSerializable, \SeekableIterator
+class ResultIterator implements ResultIteratorInterface, \JsonSerializable
 {
-    private $position;
-    protected $result;
+    private     $position;
+    protected   $result;
 
     /**
      * __construct

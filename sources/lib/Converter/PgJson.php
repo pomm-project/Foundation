b23d4f3fd26f2c6883250b4ac1365299cbde4088
Tobias Lode <tobias@beibob.de>
2017-02-28 10:39:45 +0100

    Clarify converter exception message

--- sources/lib/Converter/PgJson.php
+++ sources/lib/Converter/PgJson.php
@@ -57,7 +57,7 @@ class PgJson implements ConverterInterface
         if (JSON_ERROR_NONE !== json_last_error()) {
             throw new ConverterException(
                 sprintf(
-                    "Could not convert Json to PHP %s, driver said '%s'.\n%s",
+                    "Could not convert Json to PHP %s, json_last_error() returned '%d'.\n%s",
                     $this->is_array ? 'array' : 'object',
                     json_last_error(),
                     $data
